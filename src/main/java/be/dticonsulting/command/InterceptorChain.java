3526d42d4e892f450cc8c9a6830d75d2fabdedef
Domenique Tilleuil <Domenique.Tilleuil@gmail.com>
2015-05-23 11:09:42 +0200

    adding some javadoc

--- src/main/java/be/dticonsulting/command/InterceptorChain.java
+++ src/main/java/be/dticonsulting/command/InterceptorChain.java
@@ -1,6 +1,17 @@
 package be.dticonsulting.command;
 
+/**
+ * The interceptorChain is used by an interceptor to proceed the chain.
+ *
+ * @param <ReturnType> The Type of the return object of the command.
+ */
 public interface InterceptorChain<ReturnType> {
 
-	ReturnType proceed() throws Throwable;
+  /**
+   * Method which is supposed to be called by the interceptor the advance in the chain.
+   *
+   * @return The return object of the command.
+   * @throws Throwable The exception received from the command if it failed.
+   */
+  ReturnType proceed() throws Throwable;
 }
