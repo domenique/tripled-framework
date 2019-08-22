d6dd34f77c7e153a8f89eb6921874e8cc5af2e1d
Doménique Tilleuil <domenique.tilleuil@gmail.com>
2019-08-22 13:45:14 +0200

    fixing jacoco.

---
+++ eventbus-autoconfigure/src/test/java/eu/tripledframework/eventbus/autoconfigure/TestTheTest.java
@@ -0,0 +1,12 @@
+package eu.tripledframework.eventbus.autoconfigure;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestTheTest {
+
+    @Test
+    public void name() {
+        Assert.assertEquals(true, true);
+    }
+}
