54d4aef9cfe4d81f61980b18de125e6ae73dd659
Doménique Tilleuil <domenique.tilleuil@vlaanderen.be>
2021-01-29 22:54:32 +0100

    removed boolean for async event handling.
    The async event bus always processes events in an async fashion

--- eventbus-core/src/main/java/eu/tripledframework/eventbus/internal/domain/AsynchronousEventBus.java
+++ eventbus-core/src/main/java/eu/tripledframework/eventbus/internal/domain/AsynchronousEventBus.java
@@ -24,88 +24,74 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
- * An Implementation of the CommandDispatcher which executes the command in an asynchronous fashion.
- * This implementation uses a ThreadPool to dispatch events to separate threads.
+ * An Implementation of the CommandDispatcher which executes the command and events in an asynchronous fashion.
+ * This implementation uses a ThreadPool to dispatch command and events to separate threads.
  */
 public class AsynchronousEventBus extends SynchronousEventBus {
 
-  private final Logger logger = LoggerFactory.getLogger(AsynchronousEventBus.class);
+    private final Logger logger = LoggerFactory.getLogger(AsynchronousEventBus.class);
 
-  private Executor executor;
-  private final boolean asyncEventHandling;
+    private Executor executor;
 
-  public AsynchronousEventBus(InvokerRepository invokerRepository, InterceptorChainFactory interceptorChainFactory,
-                              List<InvokerFactory> invokerFactories, UnitOfWorkFactory unitOfWorkFactory) {
-    super(invokerRepository, interceptorChainFactory, invokerFactories, unitOfWorkFactory);
-    executor = Executors.newCachedThreadPool();
-    asyncEventHandling = false;
-  }
-
-  public AsynchronousEventBus(InvokerRepository invokerRepository, InterceptorChainFactory interceptorChainFactory,
-                              List<InvokerFactory> invokerFactories, UnitOfWorkFactory unitOfWorkFactory, Executor executor) {
-    super(invokerRepository, interceptorChainFactory, invokerFactories, unitOfWorkFactory);
-    this.executor = executor;
-    asyncEventHandling = false;
-  }
-
-  public AsynchronousEventBus(InvokerRepository invokerRepository, InterceptorChainFactory interceptorChainFactory,
-                              List<InvokerFactory> invokerFactories, UnitOfWorkFactory unitOfWorkFactory, Executor executor, boolean asyncEventHandling) {
-    super(invokerRepository, interceptorChainFactory, invokerFactories, unitOfWorkFactory);
-    this.executor = executor;
-    this.asyncEventHandling = asyncEventHandling;
-  }
-
-  @Override
-  protected <ReturnType> void dispatchInternal(Object message, CommandCallback<ReturnType> callback, UnitOfWork unitOfWork) {
-    executor.execute(new RunnableCommand<>(message, callback, unitOfWork));
-  }
-
-  @Override
-  protected void publishInternal(Object event, UnitOfWork unitOfWork) {
-    if (asyncEventHandling) {
-      executor.execute(new RunnablePublish(event, unitOfWork));
-    } else {
-      super.publishInternal(event, unitOfWork);
+    public AsynchronousEventBus(InvokerRepository invokerRepository, InterceptorChainFactory interceptorChainFactory,
+                                List<InvokerFactory> invokerFactories, UnitOfWorkFactory unitOfWorkFactory) {
+        super(invokerRepository, interceptorChainFactory, invokerFactories, unitOfWorkFactory);
+        executor = Executors.newCachedThreadPool();
     }
-  }
 
-  private class RunnableCommand<ReturnType> implements Runnable {
-
-    private final Object message;
-    private final CommandCallback<ReturnType> callback;
-    private final UnitOfWork unitOfWork;
-
-    public RunnableCommand(Object message, CommandCallback<ReturnType> callback, UnitOfWork unitOfWork) {
-      this.message = message;
-      this.callback = callback;
-      this.unitOfWork = unitOfWork;
+    public AsynchronousEventBus(InvokerRepository invokerRepository, InterceptorChainFactory interceptorChainFactory,
+                                List<InvokerFactory> invokerFactories, UnitOfWorkFactory unitOfWorkFactory, Executor executor) {
+        super(invokerRepository, interceptorChainFactory, invokerFactories, unitOfWorkFactory);
+        this.executor = executor;
     }
 
     @Override
-    public void run() {
-      AsynchronousEventBus.super.dispatchInternal(message, callback, unitOfWork);
-    }
-  }
-
-  private class RunnablePublish implements Runnable {
-
-    private UnitOfWork unitOfWork;
-    private Object event;
-
-    private RunnablePublish(Object event, UnitOfWork unitOfWork) {
-      this.unitOfWork = unitOfWork;
-      this.event = event;
+    protected <ReturnType> void dispatchInternal(Object message, CommandCallback<ReturnType> callback, UnitOfWork unitOfWork) {
+        executor.execute(new RunnableDispatch<>(message, callback, unitOfWork));
     }
 
     @Override
-    public void run() {
-      AsynchronousEventBus.super.publishInternal(event, unitOfWork);
+    protected void publishInternal(Object event, UnitOfWork unitOfWork) {
+        executor.execute(new RunnablePublish(event, unitOfWork));
     }
-  }
+
+    private class RunnableDispatch<ReturnType> implements Runnable {
+
+        private final Object message;
+        private final CommandCallback<ReturnType> callback;
+        private final UnitOfWork unitOfWork;
+
+        public RunnableDispatch(Object message, CommandCallback<ReturnType> callback, UnitOfWork unitOfWork) {
+            this.message = message;
+            this.callback = callback;
+            this.unitOfWork = unitOfWork;
+        }
+
+        @Override
+        public void run() {
+            AsynchronousEventBus.super.dispatchInternal(message, callback, unitOfWork);
+        }
+    }
+
+    private class RunnablePublish implements Runnable {
+
+        private UnitOfWork unitOfWork;
+        private Object event;
+
+        private RunnablePublish(Object event, UnitOfWork unitOfWork) {
+            this.unitOfWork = unitOfWork;
+            this.event = event;
+        }
+
+        @Override
+        public void run() {
+            AsynchronousEventBus.super.publishInternal(event, unitOfWork);
+        }
+    }
 
 
-  @Override
-  protected Logger getLogger() {
-    return logger;
-  }
+    @Override
+    protected Logger getLogger() {
+        return logger;
+    }
 }
